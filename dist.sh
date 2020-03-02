e5fe262713df726490835cb3ce0e88a4dcfa1835
Brave New World Proxy <bnw.proxy@icloud.com>
2020-03-01 20:03:35 -0500

    update client and add gui support

--- dist.sh
+++ dist.sh
@@ -1,22 +1,39 @@
+# cli clients download
 rm -rf trojan-client/
 mkdir trojan-client
-linux_download_address=https://github.com/trojan-gfw/trojan/releases/download/v1.13.0/trojan-1.13.0-linux-amd64.tar.xz
-mac_download_address=https://github.com/trojan-gfw/trojan/releases/download/v1.13.0/trojan-1.13.0-macos.zip
-win_download_address=https://github.com/trojan-gfw/trojan/releases/download/v1.13.0/trojan-1.13.0-win.zip
-
-wget $linux_download_address -O linux.tar.xz
-wget $mac_download_address -O mac.zip
-wget $win_download_address -O windows.zip
+cli_linux_download_address=https://github.com/trojan-gfw/trojan/releases/download/v1.14.1/trojan-1.14.1-linux-amd64.tar.xz
+cli_mac_download_address=https://github.com/trojan-gfw/trojan/releases/download/v1.14.1/trojan-1.14.1-macos.zip
+cli_win_download_address=https://github.com/trojan-gfw/trojan/releases/download/v1.14.1/trojan-1.14.1-win.zip
+wget $cli_linux_download_address -O linux.tar.xz
+wget $cli_mac_download_address -O mac.zip
+wget $cli_win_download_address -O windows.zip
 tar xf linux.tar.xz -C trojan-client
-mv -f trojan-client/trojan trojan-client/linux
+mkdir -p trojan-client/linux/cli
+mv -f trojan-client/trojan/* trojan-client/linux/cli
 unzip mac.zip -d trojan-client
-mv -f trojan-client/trojan trojan-client/mac
+mkdir -p trojan-client/mac/cli
+mv -f trojan-client/trojan/* trojan-client/mac/cli
 unzip windows.zip -d trojan-client
-mv -f trojan-client/trojan trojan-client/windows
+mkdir -p trojan-client/windows/cli
+mv -f trojan-client/trojan/* trojan-client/windows/cli
 rm linux.tar.xz
 rm mac.zip
 rm windows.zip
 
+# gui clients download
+cli_linux_download_address=https://github.com/TheWanderingCoel/Trojan-Qt5/releases/download/v0.0.4a/Trojan-Qt5-Linux.AppImage
+cli_mac_download_address=https://github.com/TheWanderingCoel/Trojan-Qt5/releases/download/v0.0.4a/Trojan-Qt5-macOS.dmg
+cli_win_download_address=https://github.com/TheWanderingCoel/Trojan-Qt5/releases/download/v0.0.4a/Trojan-Qt5-Windows.zip
+wget $cli_linux_download_address -P trojan-client/linux/gui/
+wget $cli_mac_download_address -P trojan-client/mac/gui/
+wget $cli_win_download_address -O windowsgui.zip
+unzip windowsgui.zip -d trojan-client/trojan
+mkdir -p trojan-client/windows/gui
+mv -f trojan-client/trojan/* trojan-client/windows/gui
+rm windowsgui.zip
+
+# clean up
+rm trojan-client/trojan -r
 zip trojan-client.zip -r trojan-client/
 rm -rf trojan-client/
 
@@ -30,3 +47,4 @@ cd ..
 mv web/web.zip dist/web.zip
 
 # tun2socks-linux-amd64 -tunAddr 10.0.0.2 -tunGw 10.0.0.1 -proxyType socks -proxyServer 192.168.1.174:1080
+# https://github.com/TheWanderingCoel/Trojan-Qt5/releases/download/v0.0.4a/Trojan-Qt5-Linux.AppImage
--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 git config user.name "Brave New World Proxy"
 git config user.email "bnw.proxy@icloud.com." 
-git config --global credential.github.com.useHttpPath true
+git config credential.github.com.useHttpPath true
